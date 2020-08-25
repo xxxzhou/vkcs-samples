a149bc3cdf60f003e77db7c998e0f0a47a0b9e61
xxxzhou <mfjt55@163.com>
2020-08-25 08:59:34 +0800

    基本库提交

---
+++ code/vkcs1/cs.cpp
@@ -0,0 +1,27 @@
+#include <util_init.hpp>
+
+int main(int argc, char *argv[]) {
+    sample_info info = {};
+    char sample_title[] = "Compute shader Sample";
+    bool depthPresent = false;
+
+    init_instance_extension_names(info);
+    init_device_extension_names(info);
+    init_instance(info, sample_title);
+    init_enumerate_device(info);
+    init_window_size(info, 500, 500);
+    init_connection(info);
+    init_window(info);
+    init_swapchain_extension(info);
+    init_device(info);
+    init_command_pool(info);
+    init_command_buffer(info);
+    execute_begin_command_buffer(info);
+    init_device_queue(info);
+    init_swap_chain(info);
+    init_depth_buffer(info);
+    init_uniform_buffer(info);
+    init_renderpass(info, depthPresent);
+    init_framebuffers(info, depthPresent);
+    return 0;
+}
