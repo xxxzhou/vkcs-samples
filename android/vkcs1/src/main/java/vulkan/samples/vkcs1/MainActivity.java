5078008200bd821815dc399608c142de2af6990a
xxxzhou <mfjt55@163.com>
2020-09-16 15:59:19 +0800

    简单android工程适配

---
+++ android/vkcs1/src/main/java/vulkan/samples/vkcs1/MainActivity.java
@@ -0,0 +1,12 @@
+package vulkan.samples.vkcs1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
